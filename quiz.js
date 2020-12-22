57a37227247ff4a53043c03c9354b3edd4d2cb86
Hanabi <hanabi1a21a2@gmail.com>
2020-12-22 23:23:41 +0900

    quizボタンを実装

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 'use strict';
-let count = 0;  // クイズの正解数
+
 
 // 質問と答えを配列で格納
 let quiz = [
@@ -17,16 +17,21 @@ let quiz = [
   },
 ];
 
-// クイズを繰り返し表示
-for (let i = 0; i < quiz.length; i++) {
-  let your_answer = prompt(quiz[i].question);
-  if (your_answer == quiz[i].answer) {
-    console.log("正解！");
-    count++;
-  } else {
-    console.log("はずれ！");
+
+document.getElementById("btn").onclick = function() {
+  // クイズを繰り返し表示
+  let count = 0;  // クイズの正解数
+  for (let i = 0; i < quiz.length; i++) {
+    let your_answer = prompt(quiz[i].question);
+    if (your_answer == quiz[i].answer) {
+      console.log("正解！");
+      count++;
+    } else {
+      console.log("はずれ！");
+    }
   }
+  document.write(quiz.length + "問中、" + count + "問正解でした！");
 }
 
+
 // ゲームの結果を表示する
-console.log(quiz.length + "問中、" + count + "問正解でした！");
